25ba9b474b0b297107a50a5ff69fa736509f3957
Trần Đức Thắng <ducthang4825@gmail.com>
2022-04-19 10:50:29 +0700

    Add project files.

---
+++ ADONET/CBBItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONET
+{
+     class CBBItem
+    {
+        private int _Value;
+        private string _Text;
+
+        public int Value { get => _Value; set => _Value = value; }
+        public string Text { get => _Text; set => _Text = value; }
+        public override string ToString()
+        {
+            return _Text;
+        }
+    }
+}
---
+++ ADONET/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ADONET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//1CONNECTION 
+            string s = @"Data Source=DESKTOP-737MH8F\SQLEXPRESS;Initial Catalog=SQLDemo;Integrated Security=True";
+            SqlConnection cnn = new SqlConnection(s);
+            //cnn.Open();
+            //cnn.Close();
+            // MessageBox.Show(cnn.State.ToString());
+            // cnn.Close();
+
+
+
+            //2COMMAND 
+            //ExecuteScalar trả về các bản ghi bị tác động trong trường hợp ko thay đổi bản ghi
+            //==> ĐẾM 
+            /* string query = "select count(*) from SV";
+             SqlCommand cmd = new SqlCommand(query,cnn);
+             cnn.Open();
+             int n = (int)cmd.ExecuteScalar();
+             MessageBox.Show(n.ToString());
+             cnn.Close();*/
+
+
+            //ExecuteNonQuery trả về các bản ghi bị tác động trong trường hợp bản ghi bị
+            //thay đổi bản ghi ==> insert , delete, update
+
+            /*string query = "INSERT INTO SV values('102200389','thang','100','2')";
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            cnn.Open();
+           cmd.ExecuteNonQuery();
+            cnn.Close();*/
+
+            /* string query = "DELETE FROM SV WHERE NameSV = 'paa'";
+             SqlCommand cmd = new SqlCommand(query, cnn);
+             cnn.Open();
+             cmd.ExecuteNonQuery();
+             cnn.Close();*/
+
+            /*string query = "UPDATE SV " +
+                "SET NameSV ='Dac Thai' ,DTB = '10' " +
+                "WHERE MSSV='102200387'";
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();*/
+
+
+            /* //ExecuteReader --> dùng cho select .... where
+             string query = "select * from LopSH";
+             SqlCommand cmd = new SqlCommand(query, cnn);
+             cnn.Open();
+             SqlDataReader r = cmd.ExecuteReader();
+             while(r.Read())
+             {           
+              comboBox1.Items.Add(new CBBItem
+                 {
+                     Value = Convert.ToInt32(r["ID_Lop"].ToString()),
+                     Text = r["NameLop"].ToString()
+             });
+             }
+             cnn.Close();*/
+
+
+
+
+            //Dataset --> mảng nhiều Datatable 
+            //cầu nối trung gian CSDL <-> DataSet
+            //SqlDataAdapter  Fill :  đổ dữ liệu từ CSDL --> Dataset
+
+            string query = "select * from SV";
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex >= 0)
+            {
+                string s = ((CBBItem)comboBox1.SelectedItem).Value.ToString()
+                    + ((CBBItem)comboBox1.SelectedItem).Text;
+                MessageBox.Show(s);
+            }
+        }
+    }
+}
